43c698ae9eb59887b8166a51f1a264293a0924ad
croissantderp <70451529+croissantderp@users.noreply.github.com>
2021-02-19 22:56:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# isTenThousand
+ a c# version of the amazing "is-ten-thousand" created by James Whitely
---
+++ main.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace isTenThousand
+{
+    public class main
+    {
+        string number1 = "";
+        string number2 = "";
+        string number3 = "";
+        string number4 = "";
+        string number5 = "";
+        string number6 = "";
+        string number7 = "";
+        string number8 = "";
+        string number9 = "";
+
+        // This is probably a bad idea
+        public bool isTenThousand(string number)
+        {
+            int iterationnumber =
+                10000
+                ;
+
+            iterationnumber =
+                iterationnumber
+                -
+                (10000 - number.Length)
+                ;
+
+            ArrayList thelongestnameIcanThinkOf =
+                new ArrayList()
+                ;
+
+            foreach (char a in number)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    ArrayList assembleTHEstring =
+                        new ArrayList();
+
+                    foreach (char ifyouREADING in iterationnumber.ToString().ToString())
+                    {
+                        int dfuadsfkuyfadskbuhvysadfuykbhvsadf =
+                            0
+                            ;
+                        for (; dfuadsfkuyfadskbuhvysadfuykbhvsadf < 1; dfuadsfkuyfadskbuhvysadfuykbhvsadf++)
+                        {
+                            assembleTHEstring.Add(ifyouREADING);
+
+
+
+
+
+
+                        }
+
+                    }
+
+                    string[] grian = (string[])assembleTHEstring.ToArray();
+
+                    string stringValuecaluesVALUESVALUES2 = String.Concat(grian);
+
+                    string stringValuecaluesVALUESVALUES = stringValuecaluesVALUESVALUES2;
+
+                    var TEN = new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }.Aggregate(0, (c, b) => c + b);
+
+                    number1 = stringValuecaluesVALUESVALUES2;
+                    string number99999999 = (
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01 +
+                        0.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01
+                        ).ToString();
+
+                    if (number99999999 == TEN.ToString().ToString())
+                    {
+                        Random random = new Random();
+
+                        int lol = random.Next();
+                        number2 = (lol + (10 - lol)).ToString();
+                    }
+
+
+                    Expression<Func<int, int>> square = x => x;
+
+                    thelongestnameIcanThinkOf.Add(int.Parse(a.ToString()) * (iterationnumber));
+                }
+            }
+
+
+
+            return true;
+        }
+
+        void smartPower()
+        {
+
+        }
+
+
+
+
+
+    }
+}
